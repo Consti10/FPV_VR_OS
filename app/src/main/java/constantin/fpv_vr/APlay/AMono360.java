28cf033922b01b9cf394c87d97ff4b76e5fa5abc
Consti10 <constantin.geier@lrz.de>
2019-09-09 14:32:15 +0200

    add 360 activities

---
+++ app/src/main/java/constantin/fpv_vr/APlay/AMono360.java
@@ -0,0 +1,16 @@
+package constantin.fpv_vr.APlay;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import constantin.fpv_vr.R;
+
+public class AMono360 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_amono360);
+    }
+}
