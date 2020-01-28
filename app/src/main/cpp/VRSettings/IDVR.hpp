1365cf137df6f238bb88a19eb149af52c5b57d27
Consti10 <geierconstantinabc@gmail.com>
2020-01-28 15:53:57 +0100

    android vr mode causes issues without GvrLayout

--- app/src/main/cpp/VRSettings/IDVR.hpp
+++ app/src/main/cpp/VRSettings/IDVR.hpp
@@ -1,5 +1,3 @@
-//Created by SettingsParser on ...
-
 #ifndef CONSTI_10_100_IDVR
 #define CONSTI_10_100_IDVR
 
@@ -18,12 +16,9 @@ public:
     static constexpr const char* DisableVSYNC="DisableVSYNC";
     static constexpr const char* Disable60FPSLock="Disable60FPSLock";
     static constexpr const char* MultiSampleAntiAliasing="MultiSampleAntiAliasing";
-    static constexpr const char* DISABLE_LOW_PERSISTENCE="Enable low persistence mode";
-    static constexpr const char* VR_InterpupilaryDistance="VR_InterpupilaryDistance";
-    static constexpr const char* VR_SceneScale="VR_SceneScale";
-    static constexpr const char* VR_DistortionCorrection="VR_DistortionCorrection";
-    static constexpr const char* VR_DISTORTION_CORRECTION_MODE="VR_DISTORTION_CORRECTION_MODE";
+    static constexpr const char* ENABLE_LOW_PERSISTENCE="Enable low persistence mode";
     static constexpr const char* VR_SCENE_SCALE_PERCENTAGE="VR_SCENE_SCALE_PERCENTAGE";
+    static constexpr const char* VR_DISTORTION_CORRECTION_MODE="VR_DISTORTION_CORRECTION_MODE";
 };
 
 #endif //CONSTI_10_100_IDVR
