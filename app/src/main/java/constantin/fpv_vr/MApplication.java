62b4150a1253c63e53256e6cfb6c4a5f02596625
Consti10 <geierconstantinabc@gmail.com>
2020-04-08 14:44:48 +0200

    add dji sdk

---
+++ app/src/main/java/constantin/fpv_vr/MApplication.java
@@ -0,0 +1,16 @@
+package constantin.fpv_vr;
+
+import android.app.Application;
+import android.content.Context;
+
+import com.secneo.sdk.Helper;
+
+public class MApplication extends Application {
+
+    @Override
+    protected void attachBaseContext(Context paramContext) {
+        super.attachBaseContext(paramContext);
+        Helper.install(MApplication.this);
+    }
+
+}
