e2613246604de8528f35727406831877f592b682
Consti10 <geierconstantinabc@gmail.com>
2020-04-14 12:58:10 +0200

    add dji sdk

--- app/src/main/java/constantin/fpv_vr/XDJI/DJIVideoPlayerSurfaceHolder.java
+++ app/src/main/java/constantin/fpv_vr/XDJI/DJIVideoPlayerSurfaceHolder.java
@@ -27,7 +27,6 @@ public class DJIVideoPlayerSurfaceHolder implements SurfaceHolder.Callback ,Vide
         this.context=context;
         VideoSettings.setVS_SOURCE(context, VideoPlayer.VS_SOURCE.EXTERNAL);
         videoPlayer=new VideoPlayer(context,null);
-
         final BaseProduct product = DJISDKManager.getInstance().getProduct();
         if (product == null || !product.isConnected()) {
             Toaster.makeToast(context,"Cannot start video",true);
@@ -46,48 +45,40 @@ public class DJIVideoPlayerSurfaceHolder implements SurfaceHolder.Callback ,Vide
 
     @Override
     public void surfaceCreated(SurfaceHolder holder) {
-
+        //Create a fake DJICodecManager to receive live video data
+        mCodecManager=new DJICodecManager(context,new SurfaceTexture(0),1280,720);
+        mCodecManager.cleanSurface();
+        videoPlayer.addAndStartDecoderReceiver(holder.getSurface());
     }
 
     @Override
     public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-        if(mCodecManager==null){
-            System.out.println("W H"+width+" "+height);
-            //mCodecManager = new DJICodecManager(context,holder,width,height);
-            mCodecManager=new DJICodecManager(context,new SurfaceTexture(0),1280,720);
-            //mCodecManager.switchSource(DJICodecManager.VideoSource.FPV);
-            mCodecManager.setOnVideoSizeChangedListener(new DJICodecManager.OnVideoSizeChangedListener() {
-                @Override
-                public void onVideoSizeChanged(int w, int h) {
-                    if(iVideoParamsChanged!=null){
-                        iVideoParamsChanged.onVideoRatioChanged(w,h);
-                        Toaster.makeToast(context,"Video w h"+w+" "+h,false);
-                    }
-                }
-            });
-            mCodecManager.cleanSurface();
-            videoPlayer.addAndStartDecoderReceiver(holder.getSurface());
-            //mCodecManager.cleanSurface();
-            //mCodecManager.destroyCodec();
-        }
     }
 
     @Override
     public void surfaceDestroyed(SurfaceHolder holder) {
-        if (mCodecManager != null) {
-            mCodecManager.destroyCodec();
-            VideoFeeder.getInstance().getPrimaryVideoFeed().removeVideoDataListener(this);
-            mCodecManager = null;
-        }
+        mCodecManager.destroyCodec();
+        VideoFeeder.getInstance().getPrimaryVideoFeed().removeVideoDataListener(this);
+        mCodecManager = null;
         videoPlayer.stopAndRemoveReceiverDecoder();
     }
 
     @Override
     public void onReceive(byte[] videoBuffer, int size) {
-        System.out.println("Data arrived");
-        if (mCodecManager != null) {
-            mCodecManager.sendDataToDecoder(videoBuffer, size);
-        }
+        //System.out.println("Data arrived");
         VideoPlayer.nativePassNALUData(videoPlayer.getNativeInstance(),videoBuffer,0,size);
     }
 }
+
+//if (mCodecManager != null) {
+//    mCodecManager.sendDataToDecoder(videoBuffer, size);
+//}
+//mCodecManager.setOnVideoSizeChangedListener(new DJICodecManager.OnVideoSizeChangedListener() {
+//                @Override
+//                public void onVideoSizeChanged(int w, int h) {
+//                    if(iVideoParamsChanged!=null){
+//                        iVideoParamsChanged.onVideoRatioChanged(w,h);
+//                        Toaster.makeToast(context,"Video w h"+w+" "+h,false);
+//                    }
+//                }
+//            });
--- app/src/main/java/constantin/fpv_vr/XDJI/DJIVideoPlayerSurfaceTexture.java
+++ app/src/main/java/constantin/fpv_vr/XDJI/DJIVideoPlayerSurfaceTexture.java
@@ -12,53 +12,45 @@ import androidx.lifecycle.OnLifecycleEvent;
 import constantin.fpv_vr.Toaster;
 import constantin.video.core.ISurfaceTextureAvailable;
 import constantin.video.core.IVideoParamsChanged;
+import constantin.video.core.VideoPlayer.VideoPlayer;
+import constantin.video.core.VideoPlayer.VideoSettings;
 import dji.common.product.Model;
 import dji.sdk.base.BaseProduct;
 import dji.sdk.camera.VideoFeeder;
 import dji.sdk.codec.DJICodecManager;
 import dji.sdk.sdkmanager.DJISDKManager;
 
-public class DJIVideoPlayerSurfaceTexture implements LifecycleObserver, ISurfaceTextureAvailable {
+public class DJIVideoPlayerSurfaceTexture implements LifecycleObserver, ISurfaceTextureAvailable, VideoFeeder.VideoDataListener {
     //Used for Android lifecycle and executing callback on the UI thread
     private final AppCompatActivity parent;
     private final Context context;
     //null in the beginning, becomes valid in the future via onSurfaceTextureAvailable
     //(Constructor of Surface takes SurfaceTexture)
     private SurfaceTexture surfaceTexture;
-    private final VideoFeeder.VideoDataListener mReceivedVideoDataListener;
+    private Surface mVideoSurface;
     private DJICodecManager mCodecManager = null;
-    private IVideoParamsChanged iVideoParamsChanged;
+    private VideoPlayer videoPlayer;
 
     public DJIVideoPlayerSurfaceTexture(final AppCompatActivity parent){
         this.parent=parent;
         this.context=parent;
-        //videoPlayer=new VideoPlayer(parent,null);
-        mReceivedVideoDataListener = new VideoFeeder.VideoDataListener() {
-            @Override
-            public void onReceive(byte[] videoBuffer, int size) {
-                if (mCodecManager != null) {
-                    System.out.println("Data");
-                    mCodecManager.sendDataToDecoder(videoBuffer, size);
-                }
-            }
-        };
+        VideoSettings.setVS_SOURCE(context, VideoPlayer.VS_SOURCE.EXTERNAL);
+        videoPlayer=new VideoPlayer(context,null);
+
         final BaseProduct product = DJISDKManager.getInstance().getProduct();
         if (product == null || !product.isConnected()) {
             Toaster.makeToast(context,"Cannot start video",true);
         } else {
-            if (!product.getModel().equals(Model.UNKNOWN_AIRCRAFT)) {
-                VideoFeeder.getInstance().getPrimaryVideoFeed().addVideoDataListener(mReceivedVideoDataListener);
-                Toaster.makeToast(context,"Start feeder",true);
-            }else{
-                Toaster.makeToast(context,"Unknown aircraft",true);
-            }
+            VideoFeeder.getInstance().getPrimaryVideoFeed().addVideoDataListener(this);
+            Toaster.makeToast(context,"Start feeder",true);
         }
         parent.getLifecycle().addObserver(this);
     }
 
 
     public void setIVideoParamsChanged(final IVideoParamsChanged vpc){
-        this.iVideoParamsChanged=vpc;
+        videoPlayer.setIVideoParamsChanged(vpc);
+        //this.iVideoParamsChanged=vpc;
     }
     public long GetExternalGroundRecorder(){
         return 0;
@@ -69,34 +61,15 @@ public class DJIVideoPlayerSurfaceTexture implements LifecycleObserver, ISurface
     public void onSurfaceTextureAvailable(final SurfaceTexture surfaceTexture) {
         //To avoid race conditions always start and stop the Video player on the UI thread
         this.surfaceTexture=surfaceTexture;
-        /*surfaceTexture.setOnFrameAvailableListener(new SurfaceTexture.OnFrameAvailableListener() {
-            @Override
-            public void onFrameAvailable(SurfaceTexture surfaceTexture) {
-
-            }
-        });*/
         parent.runOnUiThread(new Runnable() {
             @Override
             public void run() {
                 //If the callback gets called after the application was paused / destroyed
                 //(which is possible because the callback was originally not invoked on the UI thread )
                 //only create the Surface for later use. The next onResume() event will re-start the video
-                //mVideoSurface=new Surface(surfaceTexture);
+                mVideoSurface=new Surface(surfaceTexture);
                 if(DJIVideoPlayerSurfaceTexture.this.parent.getLifecycle().getCurrentState().isAtLeast(Lifecycle.State.RESUMED)){
-                    if(mCodecManager==null){
-                        surfaceTexture.setDefaultBufferSize(1280,720);
-                        mCodecManager = new DJICodecManager(context,surfaceTexture,1280,720);
-                        System.out.println("Decoder okay ? "+mCodecManager.isDecoderOK()+" W H"+mCodecManager.getVideoWidth()+" "+mCodecManager.getVideoHeight());
-                        mCodecManager.setOnVideoSizeChangedListener(new DJICodecManager.OnVideoSizeChangedListener() {
-                            @Override
-                            public void onVideoSizeChanged(int w, int h) {
-                                if(iVideoParamsChanged!=null){
-                                    iVideoParamsChanged.onVideoRatioChanged(w,h);
-                                    Toaster.makeToast(context,"Video w h"+w+" "+h,false);
-                                }
-                            }
-                        });
-                    }
+                    start();
                 }
             }
         });
@@ -104,31 +77,43 @@ public class DJIVideoPlayerSurfaceTexture implements LifecycleObserver, ISurface
 
     @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
     private void resume(){
-        if(surfaceTexture!=null){
-            if(mCodecManager==null){
-                surfaceTexture.setDefaultBufferSize(1280,720);
-                mCodecManager = new DJICodecManager(context,surfaceTexture,1280,720);
-                System.out.println("Decoder okay ? "+mCodecManager.isDecoderOK()+" W H"+mCodecManager.getVideoWidth()+" "+mCodecManager.getVideoHeight());
-            }
+        if(mVideoSurface!=null){
+            start();
         }
+    }
+
+    private void start(){
+        surfaceTexture.setDefaultBufferSize(1280,720);
+        mCodecManager = new DJICodecManager(context,surfaceTexture,1280,720);
+        System.out.println("Decoder okay ? "+mCodecManager.isDecoderOK()+" W H"+mCodecManager.getVideoWidth()+" "+mCodecManager.getVideoHeight());
+        mCodecManager.cleanSurface();
+        videoPlayer.addAndStartDecoderReceiver(mVideoSurface);
     }
 
     @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
     private void pause(){
-        if(surfaceTexture!=null){
-            if (mCodecManager != null) {
-                mCodecManager.cleanSurface();
-                mCodecManager = null;
-            }
+        if(mVideoSurface!=null){
+            mCodecManager.destroyCodec();
+            VideoFeeder.getInstance().getPrimaryVideoFeed().removeVideoDataListener(this);
+            mCodecManager = null;
+            videoPlayer.stopAndRemoveReceiverDecoder();
         }
     }
 
     @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
     private void destroy(){
-        //if(mVideoSurface!=null){
-        //    mVideoSurface.release();
-        //}
+        if(mVideoSurface!=null){
+            mVideoSurface.release();
+        }
     }
 
 
+    @Override
+    public void onReceive(byte[] videoBuffer,int size) {
+        //if (mCodecManager != null) {
+        //    System.out.println("Data");
+        //    mCodecManager.sendDataToDecoder(videoBuffer, size);
+        //}
+        VideoPlayer.nativePassNALUData(videoPlayer.getNativeInstance(),videoBuffer,0,size);
+    }
 }
