b2db9a9e4f793971ea563892d7ea10288b2526a4
Consti10 <geierconstantinabc@gmail.com>
2020-05-09 14:39:39 +0200

    minor changes

--- uvcintegration/src/main/jni/XTest/Android.mk
+++ uvcintegration/src/main/jni/XTest/Android.mk
@@ -27,7 +27,7 @@ LOCAL_CFLAGS += -O3 -fstrict-aliasing -fprefetch-loop-arrays
 LOCAL_LDLIBS := -L$(SYSROOT)/usr/lib -ldl
 LOCAL_LDLIBS += -llog
 LOCAL_LDLIBS += -landroid
-LOCAL_LDLIBS += -ljnigraphics
+#LOCAL_LDLIBS += -ljnigraphics
 
 LOCAL_SHARED_LIBRARIES += usb1.0 uvc
 LOCAL_STATIC_LIBRARIES +=libjpeg-turbo
