2c6eba7779d121b168d5cd2c10d37411c673e455
Consti10 <geierconstantinabc@gmail.com>
2020-05-11 12:24:34 +0200

    device filer

--- uvcintegration/src/main/java/constantin/test/UVCHelper.java
+++ uvcintegration/src/main/java/constantin/test/UVCHelper.java
@@ -7,6 +7,9 @@ import java.util.HashMap;
 import java.util.Iterator;
 
 public class UVCHelper {
+    public static final int DEVICE_CLASS_ROTG02=255;
+    public static final int DEVICE_SUBCLASS_ROTG02=2;
+
     private static final String TAG="UVCHelper";
     public static HashMap<String, UsbDevice> filterFOrUVC(final HashMap<String,UsbDevice> deviceList){
         final HashMap<String,UsbDevice> ret=new HashMap<>();
@@ -14,7 +17,7 @@ public class UVCHelper {
         while(keyIterator.hasNext()){
             final String key=keyIterator.next();
             final UsbDevice device = deviceList.get(key);
-            if(device.getDeviceClass()==255 && device.getDeviceSubclass()==2){
+            if(device.getDeviceClass()==DEVICE_CLASS_ROTG02 && device.getDeviceSubclass()==DEVICE_SUBCLASS_ROTG02){
                 Log.d(TAG,"Found okay device");
             }else{
                 Log.d(TAG,"Not UVC cl"+device.getDeviceClass()+" subcl "+device.getDeviceSubclass());
