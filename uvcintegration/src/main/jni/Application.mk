fc9db5e5810807f3853e2d91ccc9a0259d2145cd
Consti10 <geierconstantinabc@gmail.com>
2020-05-30 19:48:41 +0200

    remove uvcintegration (moved over to Video repo)

--- uvcintegration/src/main/jni/Application.mk
+++
@@ -1,9 +0,0 @@
-
-APP_CPPFLAGS += -std=c++17
-APP_STL := c++_static
-NDK_TOOLCHAIN_VERSION := clang
-
-APP_PLATFORM := android-9
-APP_ABI := armeabi-v7a arm64-v8a
-APP_OPTIM := debug
-#APP_OPTIM := release
