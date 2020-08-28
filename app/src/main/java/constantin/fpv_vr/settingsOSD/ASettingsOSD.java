f9faa62cb8a4645a33fca4231f46d1ea955db2c9
ru26kif <constantin.geier@campus.lmu.de>
2020-08-28 16:35:53 +0200

    go back to nested preference screens

--- app/src/main/java/constantin/fpv_vr/settingsOSD/ASettingsOSD.java
+++ app/src/main/java/constantin/fpv_vr/settingsOSD/ASettingsOSD.java
@@ -1,35 +1,21 @@
 package constantin.fpv_vr.settingsOSD;
 
-import android.annotation.SuppressLint;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.CheckBoxPreference;
 import android.util.Log;
-//import android.preference.PreferenceFragment;
-//import android.preference.PreferenceManager;
-
-import androidx.annotation.XmlRes;
 import androidx.appcompat.app.AppCompatActivity;
-
 import constantin.fpv_vr.R;
-import constantin.fpv_vr.Toaster;
-import constantin.renderingx.core.vrsettings.ASettingsVR;
-
-import androidx.fragment.app.Fragment;
-import androidx.preference.EditTextPreference;
-import androidx.preference.Preference;
-import androidx.preference.PreferenceFragment;
 import androidx.preference.PreferenceFragmentCompat;
 import androidx.preference.PreferenceManager;
+import androidx.preference.PreferenceScreen;
 
-public class ASettingsOSD extends AppCompatActivity implements PreferenceFragmentCompat.OnPreferenceStartFragmentCallback{
+public class ASettingsOSD extends AppCompatActivity implements PreferenceFragmentCompat.OnPreferenceStartScreenCallback{
     private static final String TAG= ASettingsOSD.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // Display the fragment as the main content.
         getSupportFragmentManager().beginTransaction()
-                .replace(android.R.id.content, new FSettingsOSD(R.xml.pref_osd))
+                .replace(android.R.id.content, new FSettingsOSD())
                 .commit();
     }
 
@@ -38,56 +24,42 @@ public class ASettingsOSD extends AppCompatActivity implements PreferenceFragmen
         super.onPause();
     }
 
+
     @Override
-    public boolean onPreferenceStartFragment(PreferenceFragmentCompat caller, Preference pref) {
-        final Bundle args = pref.getExtras();
-        //Log.d("LOL","onPreferenceStartFragment"+args.toString()+" "+pref.getFragment());
-        final FSettingsOSD fragment=new FSettingsOSD(getPreferenceFileForString(pref.getKey()));
-        fragment.setArguments(args);
-        fragment.setTargetFragment(caller, 0);
-        // Replace the existing Fragment with the new Fragment
+    public boolean onPreferenceStartScreen(PreferenceFragmentCompat caller, PreferenceScreen preferenceScreen) {
+        final FSettingsOSD f=new FSettingsOSD(preferenceScreen);
         getSupportFragmentManager().beginTransaction()
-                .replace(android.R.id.content, fragment)
+                .replace(android.R.id.content, f)
                 .addToBackStack(null)
                 .commit();
-        return true;
+        Log.d(TAG,"onPreferenceStartScreen "+preferenceScreen.getKey());
+        return false;
     }
 
-    // The resource file containing the preferences is set in the custom constructor
-    public static class FSettingsOSD extends PreferenceFragmentCompat {
-
-        public FSettingsOSD(@XmlRes int preferencesResId){
-            this.preferencesResId=preferencesResId;
+    public static class FSettingsOSD extends PreferenceFragmentCompat{
+        // If created by the custom constructor taking a PreferenceScreen the fragment will be inflated using this preferenceScreen instead
+        // Workaround for nested preference screens inside .xml file
+        private final PreferenceScreen preferenceScreen;
+        FSettingsOSD(PreferenceScreen preferenceScreen){
+            this.preferenceScreen=preferenceScreen;
         }
-
-        private final @XmlRes int preferencesResId;
+        FSettingsOSD(){
+            this.preferenceScreen=null;
+        }
 
         @Override
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-            Log.d(TAG,"Root key"+rootKey);
-            //setPreferencesFromResource(R.xml.pref_lol, rootKey);
             PreferenceManager preferenceManager=getPreferenceManager();
-            Log.d(TAG,"Pref name is "+preferenceManager.getSharedPreferencesName());
             preferenceManager.setSharedPreferencesName("pref_osd");
-            addPreferencesFromResource(preferencesResId);
-        }
-
-        @Override
-        public void onActivityCreated(Bundle savedInstanceState){
-            super.onActivityCreated(savedInstanceState);
+            if(preferenceScreen==null){
+                //addPreferencesFromResource(R.xml.pref_osd_elements);
+                //setPreferencesFromResource(R.xml.pref_osd_elements,rootKey);
+                addPreferencesFromResource(R.xml.pref_osd_style);
+                addPreferencesFromResource(R.xml.pref_osd_elements);
+            }else{
+                setPreferenceScreen(preferenceScreen);
+            }
         }
     }
-
-    public static @XmlRes int getPreferenceFileForString(final String s){
-        switch (s){
-            case "pref_osd_elements":return R.xml.pref_osd_elements;
-            case "pref_osd_elements1":return R.xml.pref_osd_elements1;
-
-            case "pref_osd_style":return R.xml.pref_osd_style;
-            case "pref_osd":return R.xml.pref_osd;
-        }
-        return 0;
-    }
-
 
 }
