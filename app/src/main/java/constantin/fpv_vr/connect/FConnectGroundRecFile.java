c472154a807e1738c052f1550b3d9492183a1743
ru26kif <constantin.geier@campus.lmu.de>
2020-09-06 14:28:46 +0200

    work on file select

--- app/src/main/java/constantin/fpv_vr/connect/FConnectGroundRecFile.java
+++ app/src/main/java/constantin/fpv_vr/connect/FConnectGroundRecFile.java
@@ -140,8 +140,6 @@ public class FConnectGroundRecFile extends Fragment{
                     //final String filename="TestFilename"+i;
                     final String filename = filenames.get(i);
                     final String filePath = directory + filename;
-
-                   addFileToContentProvider(filename,filePath);
                 }
             }
         });
@@ -248,23 +246,4 @@ public class FConnectGroundRecFile extends Fragment{
        }
     }
 
-
-    private void addFileToContentProvider(final String filename,final String filePathAndName) {
-        Log.d(TAG,"Add file "+filename+" "+filePathAndName);
-        ContentValues values = new ContentValues();
-        //values.put(MediaStore.Video.VideoColumns.DATE_ADDED, System.currentTimeMillis() / 1000);
-        values.put(MediaStore.Video.Media.DISPLAY_NAME, "display_name");
-        values.put(MediaStore.Video.Media.TITLE, "my_title");
-        values.put(MediaStore.MediaColumns.MIME_TYPE, "video/fpv");
-        //values.put(MediaStore.Video.Media.RELATIVE_PATH, "$Q_VIDEO_PATH/$relativePath")
-        values.put(MediaStore.Video.Media.DATA,filePathAndName);
-
-        Uri uri=mContext.getContentResolver().insert(MediaStore.Video.Media.EXTERNAL_CONTENT_URI, values);
-        if(uri!=null){
-            Log.d(TAG,"URI "+uri.toString());
-        }else{
-            Log.d(TAG,"URL is null - something went wrong");
-        }
-        //sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, Uri.parse("file://"+path+filename)));
-    }
 }
