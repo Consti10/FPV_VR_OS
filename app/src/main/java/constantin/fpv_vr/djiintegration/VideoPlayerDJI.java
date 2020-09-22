a3db9a971cda667d3941eda617b5c0ff8e046297
ru26kif <constantin.geier@campus.lmu.de>
2020-09-22 17:19:54 +0200

    move dji integration into app - fuck is this dji library stuff annoying

---
+++ app/src/main/java/constantin/fpv_vr/djiintegration/VideoPlayerDJI.java
@@ -0,0 +1,76 @@
+package constantin.fpv_vr.djiintegration;
+
+import android.content.Context;
+import android.graphics.SurfaceTexture;
+import android.util.Log;
+import android.view.Surface;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import constantin.video.core.player.VideoPlayer;
+import dji.sdk.camera.VideoFeeder;
+import dji.sdk.codec.DJICodecManager;
+import dji.sdk.products.Aircraft;
+
+// Use either one of the Interfaces to start() / stop the video player
+// e.g use either SurfaceView.getHolder().addCallback(videoPlayer); or
+// use new VideoSurfaceHolder(context,videoPlayer);
+// If enabled, receive video from a DJI drone. Else,behave same as VideoPlayer
+public class VideoPlayerDJI extends VideoPlayer {
+    private final boolean DJI_ENABLED;
+    private DJICodecManager mCodecManager;
+    private final Context context;
+    private boolean prioSet=false;
+
+    public VideoPlayerDJI(AppCompatActivity parent) {
+        super(parent);
+        this.context=parent;
+        DJI_ENABLED=DJIApplication.isDJIEnabled(context);
+        if(DJI_ENABLED){
+            final Aircraft aircraft=DJIApplication.getConnectedAircraft();
+            if (aircraft==null) {
+                Toaster.makeToast(context, "Cannot start video",true);
+                return;
+            }
+            VideoFeeder.getInstance().getPrimaryVideoFeed().addVideoDataListener(this::onReceiveDjiData);
+            Toaster.makeToast(context, "Start feeder",true);
+        }
+    }
+
+    // Order is important !
+    @Override
+    public void addAndStartDecoderReceiver(Surface surface){
+        if(DJI_ENABLED){
+            mCodecManager=new DJICodecManager(context,new SurfaceTexture(0),1280,720);
+            System.out.println("Decoder okay ? "+mCodecManager.isDecoderOK()+" W H"+mCodecManager.getVideoWidth()+" "+mCodecManager.getVideoHeight());
+            mCodecManager.cleanSurface();
+        }
+        super.addAndStartDecoderReceiver(surface);
+    }
+    // Order is important !
+    @Override
+    public void stopAndRemoveReceiverDecoder(){
+        if(DJI_ENABLED){
+            mCodecManager.destroyCodec();
+            VideoFeeder.getInstance().getPrimaryVideoFeed().removeVideoDataListener(this::onReceiveDjiData);
+            mCodecManager = null;
+        }
+        super.stopAndRemoveReceiverDecoder();
+    }
+
+
+    private void onReceiveDjiData(byte[] videoBuffer,int size) {
+        if(!prioSet){
+            Log.d("X","Thread priority"+Thread.currentThread().getPriority());
+            Thread.currentThread().setPriority(Thread.MAX_PRIORITY);
+            android.os.Process.setThreadPriority(Thread.MAX_PRIORITY);
+            prioSet=true;
+        }
+        //if (mCodecManager != null) {
+        //    System.out.println("Data");
+        //    mCodecManager.sendDataToDecoder(videoBuffer, size);
+        //}
+        VideoPlayer.nativePassNALUData(getNativeInstance(),videoBuffer,0,size);
+    }
+
+}
