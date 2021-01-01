02c8da7ed0f5e9b920e9b4f9bfa5b85c0f90b204
ru26kif <constantin.geier@campus.lmu.de>
2021-01-01 19:27:13 +0100

    slightly refactor the style of OSD

--- app/src/main/cpp/Scene/OSD/ArtificialHorizon/AHorizon.cpp
+++ app/src/main/cpp/Scene/OSD/ArtificialHorizon/AHorizon.cpp
@@ -22,6 +22,11 @@ AHorizon::AHorizon(const AHorizon::Options& options,const SettingsOSDStyle& sett
         mOptions(options){
 }
 
+void AHorizon::addOtherLadderLine(int which,std::vector<ColoredVertex>& tmpBuffOtherLadderLines,
+                                  std::vector<GLProgramText::Character>& tmpBuffOtherLadderLinesText) {
+
+}
+
 //
 void AHorizon::setupPosition() {
     mPositionDebug.setWorldPositionDebug(mX,mY,mZ,mWidth,mHeight);
@@ -29,17 +34,60 @@ void AHorizon::setupPosition() {
     //make the ladder line
     {
         const float lineH=mWidth*0.015f;
-        const float lineW=mWidth;
-        const float outline=lineH*0.5f;
-        std::vector<GLProgramLine::Vertex> tmp(6);
-        const glm::vec3 start=glm::vec3(-lineW/2.0f-outline/2.0f,-lineH/2.0f-outline/2.0f,0);
-        const glm::vec3 end=start+glm::vec3(lineW,0,0);
-        GLProgramLine::convertLineToRenderingData(start,end,lineH,tmp.data(),0,settingsOSDStyle.OSD_LINE_FILL_COLOR,settingsOSDStyle.OSD_LINE_OUTLINE_COLOR);
+        auto tmp=GLProgramLine::makeHorizontalLine({-mWidth/2.0f,0},mWidth,lineH,settingsOSDStyle.OSD_LINE_FILL_COLOR,settingsOSDStyle.OSD_LINE_OUTLINE_COLOR);
         mGLBuffLadders.uploadGL(tmp);
-
         LadderLines[0].vertOffset=0;
         LadderLines[0].vertCount=6;
+
+        /*const float lineH=mWidth*0.015f;
+        const float lineW=mWidth;
+        const float outline=lineH*0.5f;
+        //std::vector<GLProgramLine::Vertex> tmp(6);
+        const glm::vec3 start=glm::vec3(-lineW/2.0f-outline/2.0f,-lineH/2.0f-outline/2.0f,0);
+        const glm::vec3 end=start+glm::vec3(lineW,0,0);
+        //GLProgramLine::convertLineToRenderingData(start,end,lineH,tmp.data(),0,settingsOSDStyle.OSD_LINE_FILL_COLOR,settingsOSDStyle.OSD_LINE_OUTLINE_COLOR);
+        auto tmp=GLProgramLine::makeLine(start,end,lineH,settingsOSDStyle.OSD_LINE_FILL_COLOR,settingsOSDStyle.OSD_LINE_OUTLINE_COLOR);
+        mGLBuffLadders.uploadGL(tmp);
+        LadderLines[0].vertOffset=0;
+        LadderLines[0].vertCount=6;*/
     }
+    // the other ladders
+    {
+        std::vector<ColoredVertex> tmpBuffOtherLadderLines;
+        std::vector<GLProgramText::Character> tmpBuffOtherLadderLinesText;
+        /*for(int i=1;i<5;i++){
+            const float deltaBetweenLines=mHeight/180.0f*20.0f;
+            const float y=(float)i*deltaBetweenLines;
+            const float charHeight=mHeight/20.0f;
+            const std::wstring wstring=std::to_wstring(i*20);
+            const float lineWidth=mWidth-GLProgramText::getStringLength(wstring,charHeight)*2.0f;
+            ColoredGeometry::addColoredLineHorizontal(tmpBuffOtherLadderLines,{-lineWidth/2.0f, y},lineWidth,TrueColor2::GREEN);
+            //
+            GLProgramText::appendString(tmpBuffOtherLadderLinesText,0,y-(charHeight*0.5f),0,charHeight,wstring,TrueColor2::GREEN);
+        }*/
+        const float charHeight=mHeight/20.0f;
+        const float lineWidth=mWidth-GLProgramText::getStringLength(L"80",charHeight)*2.0f;
+        const float deltaBetweenLines=mHeight/180.0f;
+        int count=0;
+        for(int i=-80;i<90;i+=10){
+            count++;
+            // skip the one in the middle (this one has an outline)
+            if(i==0)continue;
+            const float y=(float)i*deltaBetweenLines;
+            if(count % 2 == 0){
+                ColoredGeometry::addColoredLineHorizontal(tmpBuffOtherLadderLines,{-lineWidth/4.0f, y},lineWidth/2.0f,settingsOSDStyle.OSD_LINE_FILL_COLOR);
+            }else{
+                const std::wstring wstring=std::to_wstring(i);
+                ColoredGeometry::addColoredLineHorizontal(tmpBuffOtherLadderLines,{-lineWidth/2.0f, y},lineWidth,settingsOSDStyle.OSD_LINE_FILL_COLOR);
+                //
+                GLProgramText::appendString(tmpBuffOtherLadderLinesText,-mWidth/2.0f,y-(charHeight*0.5f),0,charHeight,wstring,settingsOSDStyle.OSD_LINE_FILL_COLOR);
+                GLProgramText::appendString(tmpBuffOtherLadderLinesText,lineWidth/2.0f,y-(charHeight*0.5f),0,charHeight,wstring,settingsOSDStyle.OSD_LINE_FILL_COLOR);
+            }
+        }
+        mGLBuffLadderLinesOther.uploadGL(tmpBuffOtherLadderLines);
+        mGLBuffLadderLinesOtherText.uploadGL(tmpBuffOtherLadderLinesText);
+    }
+
     //make the middle triangle
     {
         float middleTriangleWidthHeight=mWidth/16.0f;
@@ -60,7 +108,9 @@ void AHorizon::setupPosition() {
 
 void AHorizon::updateGL() {
     float rollDegree= mTelemetryReceiver.getUAVTelemetryData().Roll_Deg;
-    float pitchDegree= mTelemetryReceiver.getUAVTelemetryData().Pitch_Deg;
+    //float pitchDegree= mTelemetryReceiver.getUAVTelemetryData().Pitch_Deg;
+    float pitchDegree= lol;
+    lol+=0.1;
     if(!mOptions.roll){
         rollDegree=0.0f;
     }
@@ -119,7 +169,16 @@ void AHorizon::drawGL(const glm::mat4& ViewM,const glm::mat4& ProjM) {
         mGLPrograms.line.draw(ViewM*mModelMLadders,ProjM,LadderLines[0].vertOffset,LadderLines[0].vertCount);
         mGLPrograms.line.afterDraw();
     }
-
+    //
+    glLineWidth(2.0f);
+    mGLPrograms.vc.beforeDraw(mGLBuffLadderLinesOther.getGLBufferId());
+    mGLPrograms.vc.draw(ViewM*mModelMLadders,ProjM,0,mGLBuffLadderLinesOther.getCount(),GL_LINES);
+    mGLPrograms.vc.afterDraw();
+    //
+    const glm::mat4 mvp=ProjM*(ViewM*mModelMLadders);
+    mGLPrograms.text.beforeDraw(mGLBuffLadderLinesOtherText.getGLBufferId());
+    mGLPrograms.text.draw(mvp,0,mGLBuffLadderLinesOtherText.getCount()*6);
+    mGLPrograms.text.afterDraw();
 }
 
 IPositionable::Rect2D AHorizon::calculatePosition(const IPositionable::Rect2D &osdOverlay,const int GLOBAL_SCALE) {
@@ -156,3 +215,4 @@ ColoredMeshData AHorizon::create3DModelData(float hW, float sixtW) {
     };
     return ColoredMeshData(vertices,GL_TRIANGLES);
 }
+
