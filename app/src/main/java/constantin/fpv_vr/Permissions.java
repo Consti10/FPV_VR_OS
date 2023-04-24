05d4c4d7794109044308f84d5920d21d5191f6dc
ru26kif <constantin.geier@campus.lmu.de>
2023-04-24 22:37:10 +0200

    annoying permissions api changes

--- app/src/main/java/constantin/fpv_vr/Permissions.java
+++ app/src/main/java/constantin/fpv_vr/Permissions.java
@@ -10,9 +10,8 @@ import java.util.List;
 public class Permissions {
     // These are the permissions for FPV_VR to work
     public static final String[] BASIC_PERMISSIONS=new String[]{
-            Manifest.permission.WRITE_EXTERNAL_STORAGE,
-            Manifest.permission.ACCESS_FINE_LOCATION,
-            Manifest.permission.CAMERA
+            //Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.ACCESS_FINE_LOCATION
     };
 
     public static final String[] ALL_PERMISSIONS= BASIC_PERMISSIONS;
